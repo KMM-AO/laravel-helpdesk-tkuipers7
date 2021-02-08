6a21d59d92a0889281db3a884c13440c2deefb32
tkuipers7 <tkuipers7@st.noorderpoort.nl>
2021-02-08 12:40:48 +0100

    3f

---
+++ resources/views/test/opdracht3f2.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Opdracht 3f2</h1>
+    <a href="{{route('3f1')}}">Ga naar 3f1</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// opdarcht 3a
 Route::get('/greeting', function () {
     return '<h1>Hello World</h1>';
 });
 
+//opdracht 3b
 Route::get('/dit/is/een/test', function () {
     return view('test.opdracht3b');
 });
 
+//opdracht 3c
 Route::view('/dit/is/nog/een/test','test.opdracht3b');
 
+// opdracht 3d
 Route::get('test/parameter/{id}', function ($id) {
     return view('test.opdracht3d')->with('param', $id);
 });
+
+// opdracht 3e
 Route::get('test/integer/{id}', function ($id) {
     return view('test.opdracht3d')->with('param', $id);
 })->where('id','[1-9][0-9]*');
+
+//opdacht 3f
+Route::get('/test/name/1', function () {
+   return view('test.opdracht3f1') ;
+})->name('3f1');
+Route::get('/test/name/2', function () {
+   return view('test.opdracht3f2') ;
+})->name('3f2');
+
