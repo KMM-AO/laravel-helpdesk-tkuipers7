a07053409c943f145a379b744ba85eacc561c70d
tkuipers7 <tkuipers7@st.noorderpoort.nl>
2021-02-12 11:52:33 +0100

    CHANGED Logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,3 @@
-<img src=" {{ asset('img/app_logo.png') }}" alt="" class="h-12">
+<svg xmlns="http://www.w3.org/2000/svg" width="60" viewBox="0 0 60 73.333" {{ $attributes }}>
+    <path d="M33,1A30,30,0,0,0,3,31V54.333a9.987,9.987,0,0,0,10,10H23V37.667H9.667V31a23.333,23.333,0,0,1,46.667,0v6.667H43V64.333H56.333v3.333H33v6.667H53a9.987,9.987,0,0,0,10-10V31A30,30,0,0,0,33,1Z" transform="translate(-3 -1)"/>
+</svg>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo />
+                        <x-application-logo class="block h-12 w-auto" />
                     </a>
                 </div>
 
