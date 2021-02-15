b6ed41a8f90c0df913af0559fe52eaccff8232d0
tkuipers7 <tkuipers7@st.noorderpoort.nl>
2021-02-15 18:52:55 +0100

    11b

--- resources/views/components/applicant/applicant.blade.php
+++ resources/views/components/applicant/applicant.blade.php
@@ -8,7 +8,9 @@
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
         <div class="text-sm text-gray-900">
-            {{ $applicant->user->email }}
+            <x-link :href="('mailtto:' . $applicant->user->email">
+                {{ $applicant->user->email }}
+            </x-link>
         </div>
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,7 @@
+@php
+$classes = 'text-gray-900 hover:text-gray-500';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
